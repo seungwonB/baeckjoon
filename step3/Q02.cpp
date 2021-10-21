6fad70ee0fbf2ac32fadff6da705b01b09d59c54
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-21 16:43:10 +0900

    A+B-3

---
+++ step3/Q02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	int x, y;
+	cin >> num;
+
+	for(int i=0;i<num;i++){
+		cin >> x >> y;
+		
+		cout << x + y << "\n";
+		
+	}
+	
+}
