3113c020c10cfb919484bb4518bdb6daad096a13
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-21 16:47:29 +0900

    A+B -7

---
+++ step3/Q07.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+int main() {
+	int num;
+	int x, y;
+	cin >> num;
+
+	for(int i=0;i<num;i++){
+		cin >> x >> y;
+		
+		cout << "Case #" << i+1 << ": " << x + y << "\n";
+		
+	}
+	
+}
