b98395a1e326de38141a8050ba9fcd8da874bf29
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-22 22:00:28 +0900

    A+B -4

---
+++ step4/Q02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+int main() {
+	int x, y;
+	
+	while (true) {
+		cin >> x >> y;
+		if (cin.eof()) break;
+		cout << x+y << "\n";
+	}
+}
