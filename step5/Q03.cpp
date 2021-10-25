ffdf21465c32dcb91fab231fe25302b755d51a77
tmddnjs513 <tmddnjs513@gmail.com>
2021-10-25 23:16:46 +0900

    숫자의 개수

---
+++ step5/Q03.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+//세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 
+//0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성하시오.
+//예를 들어 A = 150, B = 266, C = 427 이라면 A × B × C = 150 × 266 × 427 = 17037300 이 되고, 
+//계산한 결과 17037300 에는 0이 3번, 1이 1번, 3이 2번, 7이 2번 쓰였다.
+
+int main() {
+	int a, b, c;
+	int res;
+	int array[10] = { 0 };
+	cin >> a >> b >> c;
+	res = a * b * c;
+	
+	while(res != 0) {
+		array[res % 10]+=1;
+		res /= 10;
+	}
+	for(int i=0;i<10;i++){
+		cout << array[i] << endl;
+	}
+}
