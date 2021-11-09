f087f17aa3240492d12b59c039d5a3f9b80b9001
tmddnjs513 <tmddnjs513@gmail.com>
2021-11-09 22:43:51 +0900

    셀프 넘버

---
+++ step6/Q02.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main() {
+	int cnt = 0;
+	int array[1000];
+	int num;
+	cin >> num;
+	
+	if (num < 100) {
+		for(int i = 1; i <= num; i++) {
+			cnt++;
+		}
+	} else {
+		cnt = 99;
+		for(int i = 100; i <= num; i++){
+			int hundred = i / 100;
+			int ten = (i % 100) / 10;
+			int one = (i % 100) % 10;
+			
+			if (hundred - ten == ten - one)
+				cnt++;	
+			
+		}
+			
+	}
+	
+	cout << cnt;
+	
+
+}
