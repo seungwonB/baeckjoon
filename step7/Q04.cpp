afaefd8bef1659f041c5daec667b83b59ff3fd70
tmddnjs513 <tmddnjs513@gmail.com>
2021-11-11 14:47:49 +0900

    연습

---
+++ step7/Q04.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+// 문자열 반복 
+int main() {
+	int num;
+	int create;
+	string 	
+	cin >> num;
+	
+	for(int i=0;i<num;i++){
+		cin >> create;
+		
+	}
+} 
