f39d6a20d4c5f30c7f71b39ce9978321a7924d1f
tmddnjs513 <tmddnjs513@gmail.com>
2021-11-15 19:43:29 +0900

    단어 공부

--- step7/Q04.cpp
+++ step7/Q04.cpp
@@ -6,12 +6,19 @@ using namespace std;
 // 문자열 반복 
 int main() {
 	int num;
-	int create;
-	string 	
+	int cnt;
+	string str;
 	cin >> num;
 	
-	for(int i=0;i<num;i++){
-		cin >> create;
+	for(int i=0;i<num;i++) {
+		cin >> cnt >> str;
+		
+		for(int j=0;j<str.length();j++) {
+			for(int k=0;k<cnt;k++){
+				cout << str[j];
+			}
+		}
+		cout << "\n";
 		
 	}
 } 
