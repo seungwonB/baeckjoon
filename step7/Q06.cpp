b4d844f4c4aa0b26bdc962323d97df80c0956f5a
tmddnjs513 <tmddnjs513@gmail.com>
2021-11-16 16:53:29 +0900

    단어의 개수:1152

--- step7/Q06.cpp
+++ step7/Q06.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <cctype>
 using namespace std;
-//알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 
-//단, 대문자와 소문자를 구분하지 않는다.
+//1152번 단어의 개수 브론즈2 
+//영어 대소문자와 공백으로 이루어진 문자열이 주어진다. 이 문자열에는 몇 개의 단어가 있을까? 
+//이를 구하는 프로그램을 작성하시오. 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다. 
 
 int main() {
-	string str; // 입력 문자열
-	cin >> str;
-	string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int arr[26] = { 0 };
-	int max;
-	char ch;
-		
-	for(int i=0;i<str.length();i++) {
-		for(int j=0;j<alphabet.length();j++){
-			if (toupper(str[i]) == alphabet[j]) {
-				arr[j]++;
-			}
-		}
+	string str; // 입력 문자열 
+	int cnt = 0; // 공백 갯수  
+	getline(cin, str);
+
+	for(int i=0;i<str.length();i++){
+		if(isspace(str[i]) != 0) 
+			cnt++;
 	}
-	max = arr[0];
-	ch = 'A';
-	for(int i=0;i<26;i++){
-		if (max == arr[i]) {
-			if (i == 0 ){
-				ch = 'A';
-			}else{
-				ch = '?';	
-			}
-		}
-		if (max < arr[i]){
-			max = arr[i];
-			ch = alphabet[i];
-		} 
-//		cout << arr[i] << " ";
+	// 처음, 마지막 공백 제거 
+	if(isspace(str[0]) != 0 ){
+		cnt--;
 	}
-	cout << ch;
+	if(isspace(str[str.length()-1]) != 0 ){
+		cnt--;
+	}
+	cout << cnt + 1;
+	
 }
+
