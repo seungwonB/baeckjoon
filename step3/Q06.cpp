0d3e2b3df1a639607022376093f794b860c5988c
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2021-11-16 17:07:54 +0900

    기찍N

--- step3/Q06.cpp
+++ step3/Q06.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 
 using namespace std;
-//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
 int main() {
 	int num;
-	int x, y;
 	cin >> num;
-
-	for(int i=0;i<num;i++){
-		cin >> x >> y;
-		
-		cout << "Case #" << i+1 << ": " << x + y << "\n";
-		
+	
+	for(int i=num; i>=1; i--) {
+		cout << i << "\n";
 	}
 	
 }
