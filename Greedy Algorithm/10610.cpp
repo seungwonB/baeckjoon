ad67a1459b7ce59dd07016e58b80a775be8410f9
Seungwon Bang <73030613+seungwonB@users.noreply.github.com>
2023-08-03 23:03:32 +0900

    30

--- Greedy Algorithm/10610.cpp
+++ Greedy Algorithm/10610.cpp
@@ -1,98 +1,34 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
-#include <queue>
-#define MAX 8
+
 using namespace std;
 
-int n, m, ans = 0;
-int lab[MAX][MAX];
-int temp[MAX][MAX];
-int dy[4] = {0, 0, 1, -1};
-int dx[4] = {1, -1, 0, 0};
-bool visited[MAX][MAX];
+bool isThirty(string num) {
+	int res = 0;
+	bool flag = false;
+	
+	for(char c : num) {
+		res += c - '0';
+		if (c == '0') flag = true;
+	}
 
-void copy(int arr1[MAX][MAX], int arr2[MAX][MAX]) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			arr2[i][j] = arr1[i][j]; 
-		}
-	}
-}
-
-void bfs() {
-	int virus[MAX][MAX];
-	copy(temp, virus);
-	
-	queue<pair<int, int>> q;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (virus[i][j] == 2) 
-				q.push({i, j});
-		}
-	}
-	
-	while(!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
-		
-		for (int i = 0; i < 4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			
-			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-			if (virus[ny][nx] == 0) {
-				virus[ny][nx] = 2;
-				q.push({ny, nx});
-			}
-		}
-	}
-	
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (virus[i][j] == 0) cnt++;
-		}
-	}
-	ans = max(ans, cnt);
-}
-
-void wall(int cnt) {
-	if (cnt == 3) {
-		bfs();
-		return;
-	}
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (temp[i][j] == 0) {
-				temp[i][j] = 1;
-				wall(cnt++);
-				temp[i][j] = 0;
-			}
-		}
-	}
+	// 3의 배수이고 0이 존재해야만 30의 배수이다.	
+	return res % 3 == 0 && flag;
 }
 
 int main(){
-	cin >> n >> m;
+	string N;
+	cin >> N;
 	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> lab[i][j];
-		}
+	// 내림차순 정렬 
+	sort(N.begin(), N.end(), greater<char>());
+	
+	if (isThirty(N)) {
+		cout << N;	
+	} else {
+		cout << -1;
 	}
 	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (lab[i][j] == 0) {
-				memset(visited, 0, sizeof(visited));				
-				copy(lab, temp);
-				temp[i][j] = 1;
-				wall(1);
-				temp[i][j] = 0;
-			}
-		}
-	}
-	cout << ans;
+	return 0;
 }
